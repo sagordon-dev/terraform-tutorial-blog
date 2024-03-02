142842d0d3c1091343c193633b91044a995195f6
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-01 20:06:55 -0500

    Add alb module

---
+++ modules/alb/main.tf
@@ -0,0 +1,29 @@
+resource "aws_alb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.subnets
+  security_groups    = [var.security_group]
+
+  tags = {
+    Name = "app-lb"
+  }
+}
+
+resource "aws_alb_target_group" "app" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app.arn
+  }
+}
