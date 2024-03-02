f9552e4dbe77f0c74125a0345ab2ce03eb1a83b9
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-01 20:08:23 -0500

    Format ec2 .tf files

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "app_server" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    subnet_id = var.subnet_id
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
 
-    tags = {
-        Name = "AppServer"
-    }
+  tags = {
+    Name = "AppServer"
+  }
 }
