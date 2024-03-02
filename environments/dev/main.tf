70d1fded51c6bb3d1f2bff579c76248553f6ecce
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-02 09:54:59 -0500

    Declare variables in modules

---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  cidr_block                 = var.cidr_block
+  public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  availability_zones         = var.availability_zones
+}
+
+module "alb" {
+  source = "../../modules/alb"
+
+  security_group = alb.security_group_id
+  vpc_id         = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,13 @@
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type to use for the EC2 instances"
+  type        = string
+}
+
 variable "cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
