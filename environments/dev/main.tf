9e39a7deb4a89697155e3e075f063eb10fc9e188
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-02 21:49:15 -0500

    Update modules fixing bugs with sg etc

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,18 +1,33 @@
 module "vpc_dev" {
-  source     = "../../modules/dev"
-  cidr_block = var.vpc_cidr
+  source                     = "../../modules/vpc"
+  cidr_block                 = var.cidr_block
+  public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  availability_zones         = var.availability_zones
 }
 
 module "ec2_dev" {
-  source        = "../../modules/dev"
-  ami_id        = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = module.vpc_dev.subnets[0] # Assuming VPC module returns a list of subnets
+  source         = "../../modules/ec2"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnets        = [module.vpc_dev.subnets[0]] # Assuming VPC module returns a list of subnets 
+  subnet_id      = module.vpc_dev.subnets[0]   # Assuming VPC module returns a list of subnets
+  security_group = aws_security_group.dev_security_group.id
+}
+
+resource "aws_security_group" "dev_security_group" {
+  name_prefix = "dev-"
+  vpc_id      = module.vpc_dev.vpc_id
+
+  tags = {
+    Environment = "dev"
+  }
 }
 
 module "alb_dev" {
-  source         = "../../modules/dev"
-  subnets        = module.vpc_dev.public_subnets # Adjust based on the module's output
-  security_group = var.dev_security_group
+  source         = "../../modules/alb"
+  subnets        = module.vpc_dev.subnets
+  security_group = aws_security_group.dev_security_group.id
   vpc_id         = module.vpc_dev.vpc_id
 }
+
