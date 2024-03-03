7cfea0f710aebc46b31b0c9284192c2f2f8fb154
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-02 22:16:17 -0500

    Fix subnets residing is same AZ

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,8 +19,8 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr_blocks[0]
-  availability_zone = var.availability_zones[0]
+  cidr_block        = var.private_subnet_cidr_blocks[1]
+  availability_zone = var.availability_zones[1]
   tags = {
     Name = "Private Subnet 1"
   }
@@ -54,7 +54,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main.id
   }
 
@@ -80,6 +80,3 @@ resource "aws_nat_gateway" "main" {
     Name = "Main NAT Gateway"
   }
 }
-
-
-
