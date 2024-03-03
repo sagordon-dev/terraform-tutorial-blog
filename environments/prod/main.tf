fe8dce1f56a20cd36d85191384ef02a0f13a67e0
Scott Gordon <5c077.60rd0n@gmail.com>
2024-03-02 21:50:09 -0500

    Update module outputs

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -10,9 +10,18 @@ module "ec2_prod" {
   subnet_id     = module.vpc_dev.subnets[0] # Assuming VPC module returns a list of subnets
 }
 
+resource aws_security_group "prod_security_group" {
+  name_prefix = "prod-"
+  vpc_id      = module.vpc_dev.vpc_id
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
 module "alb_prod" {
   source         = "../../modules/prod"
   subnets        = module.vpc_dev.public_subnets # Adjust based on the module's output
-  security_group = var.prod_security_group
+  security_group = aws_security_group.prod_security_group.id
   vpc_id         = module.vpc_dev.vpc_id
 }
